091f8f484ea472b4928824368222c7cb5935aa36
diwakarjhadj <dkj9910419457@gmail.com>
2024-08-19 21:34:20 +0530

    ui-changes-Loader

--- src/components/DataTable.jsx
+++ src/components/DataTable.jsx
@@ -30,62 +30,8 @@ const DataTable = () => {
         fetchData();
     }, [state.selectedMonth,state.searchTerm]);
 
-    // const getFirstTenWords = (description) => {
-    //     const words = description.split(' ');
-    //     return words.slice(0, 55).join(' ');
-    // }
-
-
     return (
         <>
-            {/* <div className='data-table'>
-                {loading?(
-                    <div className="loader-container">
-                    <Oval
-                        height={80}
-                        width={80}
-                        color="#4fa94d"
-                        wrapperStyle={{}}
-                        wrapperClass=""
-                        visible={true}
-                        ariaLabel='oval-loading'
-                        secondaryColor="#4fa94d"
-                        strokeWidth={2}
-                        strokeWidthSecondary={2}
-                    />
-                </div>
-                ):(
-                    <table>
-                        <thead>
-                            <tr>
-                                <th className='id'>ID</th>
-                                <th className='title'>Title</th>
-                                <th className='desc'>Description</th>
-                                <th className='price'>Price</th>
-                                <th className='category'>Category</th>
-                                <th className='sold'>Sold</th>
-                                <th className='image'>Image</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {transactions?.map((transaction) => (
-                                <tr key={transaction.id}>
-                                    <td className='id'>{transaction.id}</td>
-                                    <td className='title'>{transaction.title}</td>
-                                    <td className='desc'>{transaction.description}</td>
-                                    <td className='price'>{transaction.price}</td>
-                                    <td className='category'>{transaction.category}</td>
-                                    <td className='sold'>{transaction.sold ? "Sold" : "Not Sold"}</td>
-                                    <td className='image'>
-                                        <img src={transaction.image} alt={transaction.title} style={{ width: '95%', height: '85%' }} />
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                )}
-                
-            </div> */}
             <div className='data-table'>
             {loading ? (
                 <div className="loader-container">
